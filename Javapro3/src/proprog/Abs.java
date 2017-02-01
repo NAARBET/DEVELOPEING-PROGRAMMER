97b8ec2674c37099ba7f30a19c09f4cf42b4fe5f
NAARBET <naarbetxavier@gmail.com>
2017-02-01 19:01:27 +0530

    Java using "implement"

---
+++ Javapro3/src/proprog/Abs.java
@@ -0,0 +1,13 @@
+package proprog;
+
+public abstract class Abs {
+	
+	public int add()
+	{
+		int a=1;int b=2; int c=0;
+		c=a+b;
+		return(c);
+	}
+	public abstract int sub();
+
+}
---
+++ Javapro3/src/proprog/Implementation.java
@@ -0,0 +1,22 @@
+package proprog;
+
+public class Implementation extends Abs{
+	public static void main(String[] args) {
+		Implementation s= new Implementation();
+		int f =s.add();
+		int g =s.sub();
+		System.out.println(f);
+		System.out.println(g);
+		}
+		public int sub(){
+		int a=2;
+		int b=4;
+		int c =0;
+		c=a-b;
+		return(c);
+	}
+	
+
+	
+	
+} 
