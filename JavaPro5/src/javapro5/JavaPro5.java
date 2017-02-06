d01e324165279567ad5c3c1d4764810d3feaaf08
NAARBET <naarbetxavier@gmail.com>
2017-02-06 17:19:06 +0530

    Cube of given no

---
+++ JavaPro5/src/javapro5/JavaPro5.java
@@ -0,0 +1,15 @@
+package javapro5;
+public class JavaPro5 {
+public static void main(String[] args) {
+    int a[]={4,5,6,7};
+    int c;
+        System.out.println("To get individual cube");
+        for(int i=0;i<3;i++)
+        {
+            System.out.println(a[i]);
+             c = a[i]*a[i]*a[i];
+             System.out.println("the value cube is"+ c);
+        }
+    }
+    
+}
